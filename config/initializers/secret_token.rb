61716562de2f5940b893fd66e20243ec4ba88df8
mattbarackman <matthew.barackman@gmail.com>
2012-12-03 22:46:43 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerHunt::Application.config.secret_token = '97a852f71bb905c95e6953106707c8c916e59195392d4b8058a59c4e6d5cc33a5508d7acd052d62ef4f3ae3fac0b21e596b7b4580832b2ac56e977a820676c2a'
